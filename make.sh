c9b74bc7e949d1743ef0df188e681a5e307832b7
mzyzek <mzyzek@gmail.com>
2025-05-26 21:04:55 +0200

    Script for building

---
+++ make.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo "Building ${PKG_NAME}."
+set -ex
+
+# for subpackages, we have named our extracted locations according to the subpackage name
+#    That's what this $PKG_NAME is doing - picking the right subfolder to rsync
+
+src="$SRC_DIR/$PKG_NAME"
+
+# # The license files are no longer manually packed but are included via the meta.yaml
+# # not all packages have the license file.  Copy it from mkl, where we know it exists
+# cp -f "$SRC_DIR/mkl/info/licenses/license.txt" "$SRC_DIR"
+# # ro by default.  Makes installations not cleanly removable.
+# chmod 664 "$SRC_DIR/license.txt"
+
+make
+pwd > /tmp/test
+
+cp -rv "$SRC_DIR"/a.out "$PREFIX/bin"/mzyzek-awk
+
+# Remove info coming from conda package. conda-build will provide its own metadata.
+rm -rvf "$PREFIX/info"
+
+
+
